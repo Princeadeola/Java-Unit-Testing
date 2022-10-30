6551cc6f1805c2c2d1eb2aa1455119bd30ad666f
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-30 08:30:07 -0700

    committed

---
+++ src/Mood/MoodAnalyserTest.java
@@ -0,0 +1,27 @@
+package Mood;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MoodAnalyserTest {
+    @Test
+    void testMoodAnalysis() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        assertThat(mood, CoreMatchers.is("SAD"));
+    }
+
+    @Test
+    void HappyMoods() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a happy message");
+        assertThat(mood, CoreMatchers.is("HAPPY"));
+    }
+}
+
+
+// commands from intelliJ
+//  Press Ctrl+Shift+F10 to run this individual test.
+//  Shift+F10 to re-run the last test.
+//  Run second test case by pressing Alt+Shift+R

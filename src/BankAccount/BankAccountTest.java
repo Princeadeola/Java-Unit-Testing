743bdc0f417e6d78bbdfdfce6c52124cc83ed8ca
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-31 01:11:11 -0700

    committed

--- src/BankAccount/BankAccountTest.java
+++ src/BankAccount/BankAccountTest.java
@@ -6,13 +6,18 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
+        BankAccount account = new BankAccount("Adeola", "Adesipe", 5000.00);
+        double balance = account.deposit(2500.00, true);
+        assertEquals(7500.00, balance,0);
     }
 
     @org.junit.Test
     public void withdraw() {
+        fail("This method is not yet tested");
     }
 
     @org.junit.Test
     public void getBalance() {
+        fail("This method is not yet tested");
     }
 }
